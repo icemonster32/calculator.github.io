c75e33aa20861c7059799191851e73c479e8d195
Nicolo Galinato <75724390+icemonster32@users.noreply.github.com>
2022-08-21 19:05:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let result = document.querySelector("#result")
+
+/* onload input focus */
+onload = () => {
+   result.focus()
+}
+
+/* display number in input */
+display = (value) => {
+   result.value += value
+   onload()
+}
+
+/* clear all inputs */
+allClear = () => {
+   result.value = ""
+   result.style.color = "black"
+   result.readOnly = false
+   onload()
+}
+
+/* erase num*/
+erase = () => {
+   let erase = result.value.slice(0, result.value.length - 1)
+   result.value = erase
+   onload()
+}
+
+/* compute */
+compute = () => {
+   try {
+      let calc = new Function("return " + result.value)()
+      result.value = calc
+      onload()
+   } catch (exception) {
+      result.value = "Format error!"
+      result.style.color = "red"
+      result.readOnly = true
+   }
+}
